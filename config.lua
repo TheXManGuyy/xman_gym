1c6177cb6ce60728d620b1cc069131ded05a205d
TheXManGuyy <76070163+TheXManGuyy@users.noreply.github.com>
2021-04-29 12:52:17 +0300

    Added JAIL location

--- config.lua
+++ config.lua
@@ -1,17 +1,14 @@
 Config = {}
 Config.DrawDistance = 1.0  -- The distance between you and the marker.
-Config.TrainAmmount = 2.0  -- The number of skill points you get per workout.
 
 -- Messages
-Config.GymDraw3D = '~g~[E]~w~ Do a workout'  -- A message that is displayed when you are close to an exercise.
-Config.BeginExercise = 'Getting ready for an exercise..' -- Message witch is shown when you are getting ready for exercise.
-Config.ProgressBar = 'Doing my workout'  -- Message shown on progressbar when you are doing exercise.
+Config.GymDraw3D = '~g~E~s~ - Start workout'  -- A message that is displayed when you are close to an exercise.
+Config.ProgressBar = 'Doing workout'  -- Message shown on progressbar when you are doing exercise.
 
  -- Here you can add new Gyms to your server, etc jail..
 Config.Locations = {
 
-	Gym = {
-	
+	City = {
 		Blip = {
 			Coords = vector3(-1202.9837, -1565.1718, 4.6115),
 			Name = 'Gym'
@@ -32,6 +29,28 @@ Config.Locations = {
 		Chins = {
 			vector3(-1200.1284, -1570.9903, 4.6115)
 		}
-	}
+	},
 
+	Jail = {
+		Blip = {
+			Coords = vector3(1639.233,2527.824,45.565),
+			Name = 'Gym'
+		},
+		
+		Arms = {
+			vector3(1639.233,2527.824,45.565)
+		},
+		
+		Pushup = {
+			vector3(1637.201,2524.245,45.565)
+		},
+		
+		Situps = {
+			vector3(1639.23,2532.128,45.565)
+		},
+		
+		Chins = {
+			vector3(1642.848,2528.256,45.565)
+		}
+	}	
 }
